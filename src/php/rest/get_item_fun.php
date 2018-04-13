3cf59c219398dfc768d02c8cb3bb3fe2cef610ca
BaezCrdrm <dev.baez_crdrm@outlook.com>
2018-04-13 14:03:41 -0500

    Get single item improvements. It needs more develop.

--- src/php/rest/get_item_fun.php
+++ src/php/rest/get_item_fun.php
@@ -6,35 +6,50 @@
  */
 function get_single_item($id = null)
 {
-    $query = "SELECT m.mov_id, i.item_title, t.lang, t.title, m.mov_cat, i.item_year, i.item_info 
-    FROM movies m
-    LEFT JOIN titles t ON m.mov_id = t.item_id
-    INNER JOIN items i ON m.mov_id = i.item_id ";
+    $_reltive_root_dir = "../../";
+    // Need to change in the near future
+    $query = "SELECT i.item_id, i.item_title, i.item_category, i.item_year, i.item_info, 
+    t.lang, t.title, m.mov_cat 
+    FROM items i
+    INNER JOIN movies m ON m.mov_id = i.item_id 
+    LEFT JOIN titles t ON t.item_id = m.mov_id ";
 
     if(isset($id))
     {
         $query .= "WHERE i.item_id = $id ";
     }
 
-    $query .= "GROUP BY 1,2,3,4,5,6,7";
+    $query .= "GROUP BY 1,2,3,4,5,6,7,8;";
 
-    require '../../src/php/ctrl/queries.php';
+    require $_reltive_root_dir.'src/php/ctrl/queries.php';
+    require_once $_reltive_root_dir.'src/php/model/models.php';
     $res = executeQuery($query);
-    $movies = array();
+    $items = array();
     $i = 0;
     while($reg = mysqli_fetch_array($res, MYSQLI_NUM))
     {
-        $movies[$i] = array(
-            'item_id' => $reg[0],
-            'item_title' => $reg[1],
-            'lang' => $reg[2],
-            'title' => $reg[3],
-            'mov_cat' => $reg[4],
-            'item_year' => $reg[5],
-            'item_info' => $reg[6]
-        );
+        // $reg[0], // ID
+        // $reg[1], // Item title
+        // $reg[2], // Item category
+        // $reg[3], // Item year
+        // $reg[4], // Item info
+        // $reg[5], // Item title language ID
+        // $reg[6], // Item translated title
+        // $reg[7], // Movie cat
+
+        // 1. Movies
+        if($reg[2] == 1)
+        {
+            $items[$i] = new Movie(
+                $reg[0], // ID
+                $reg[1], // Item title
+                $reg[7], // Movie cat
+                $reg[3], // Item year
+                $reg[4] // Item info
+            );
+        }
         $i++;
     }
-    return $movies;
+    return $items;
 }
 ?>
