13fe03bd0fdc1c1db5c73c1d039931b213d6712f
BaezCrdrm <dev.baez_crdrm@outlook.com>
2018-04-13 17:40:58 -0500

    Testing data

---
+++ db/test_data.sql
@@ -0,0 +1,149 @@
+USE cyg;
+INSERT INTO mov_category (2, 'Drama');
+INSERT INTO mov_category (3, 'Comedy');
+INSERT INTO mov_category (4, 'Sci-Fi');
+INSERT INTO mov_category (5, 'Horror');
+INSERT INTO mov_category (6, 'Romance');
+INSERT INTO mov_category (7, 'Action');
+INSERT INTO mov_category (8, 'Mistery');
+INSERT INTO mov_category (9, 'Crime');
+INSERT INTO mov_category (10, 'Animation');
+INSERT INTO mov_category (11, 'Fanstasy');
+INSERT INTO mov_category (12, 'Superhero');
+INSERT INTO mov_category (13, 'Sitcom');
+INSERT INTO mov_category (14, 'Documental');
+INSERT INTO mov_category (15, 'Reality show');
+INSERT INTO mov_category (16, 'Cartoon');
+INSERT INTO mov_category (17, 'Talk show');
+
+INSERT INTO items (
+    1,
+    'Star Wars',
+    1,
+    1977,
+    'http://www.imdb.com/title/tt0076759/'
+);
+
+INSERT INTO items (
+    2,
+    'Kung Fu Panda',
+    1,
+    2008,
+    'http://www.imdb.com/title/tt0441773/'
+);
+
+INSERT INTO items (
+    3,
+    'Avengers',
+    1,
+    2012,
+    'http://www.imdb.com/title/tt0848228/'
+);
+
+INSERT INTO items (
+    4,
+    'Men in Black',
+    1,
+    1997,
+    'http://www.imdb.com/title/tt0119654/'
+);
+
+INSERT INTO items (
+    5,
+    'Logan',
+    1,
+    2017,
+    'http://www.imdb.com/title/tt3315342/'
+);
+
+INSERT INTO items (
+    6,
+    'The big bang theory',
+    2,
+    2007,
+    'http://www.imdb.com/title/tt0898266/'
+);
+
+INSERT INTO items (
+    7,
+    'How I met your mother',
+    2,
+    2005,
+    'http://www.imdb.com/title/tt0460649/'
+);
+
+INSERT INTO items (
+    8,
+    'This love',
+    3,
+    2003,
+    'https://www.youtube.com/watch?v=XPpTgCho5ZA'
+);
+
+
+INSERT INTO movies (
+    1,
+    4
+);
+
+INSERT INTO movies (
+    2,
+    10
+);
+
+INSERT INTO movies (
+    3,
+    12
+);
+
+INSERT INTO movies (
+    4,
+    3
+);
+
+INSERT INTO movies (
+    5,
+    12
+);
+
+INSERT INTO movies (
+    6,
+    13
+);
+
+INSERT INTO movies (
+    7,
+    13
+);
+
+
+
+INSERT INTO titles (
+    1,
+    2,
+    'La guerra de las galaxias'
+);
+
+INSERT INTO titles (
+    3,
+    2,
+    'Los vengadores'
+);
+
+INSERT INTO titles (
+    4,
+    2,
+    'Hombres de negro'
+);
+
+INSERT INTO titles (
+    6,
+    2,
+    'La teoria del big bang'
+);
+
+INSERT INTO titles (
+    1,
+    2,
+    'Como conoci a su madre'
+);
